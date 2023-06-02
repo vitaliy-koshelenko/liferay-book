e4bcb72dc6e11c09e802a6b00e4f1e439f39a6ac
Vitaliy Koshelenko <vetal324@gmail.com>
2023-06-02 16:33:04 +0300

    05-panel-apps: JSP PanelApp

--- modules/liferay-courses-web/src/main/java/com/liferaybook/courses/web/application/list/LiferayCoursesJSPPanelApp.java
+++ modules/liferay-courses-web/src/main/java/com/liferaybook/courses/web/application/list/LiferayCoursesJSPPanelApp.java
@@ -1,6 +1,6 @@
 package com.liferaybook.courses.web.application.list;
 
-import com.liferay.application.list.BasePanelApp;
+import com.liferay.application.list.BaseJSPPanelApp;
 import com.liferay.application.list.PanelApp;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferaybook.courses.web.application.list.constants.LiferayBookPanelKeys;
@@ -8,14 +8,21 @@ import com.liferaybook.courses.web.constants.LiferayCoursesAdminPortletKeys;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
+import javax.servlet.ServletContext;
+
 @Component(
 	property = {
-		"panel.app.order:Integer=202",
+		"panel.app.order:Integer=201",
 		"panel.category.key=" + LiferayBookPanelKeys.LIFERAY_BOOK
 	},
 	service = PanelApp.class
 )
-public class LiferayCoursesPanelApp extends BasePanelApp {
+public class LiferayCoursesJSPPanelApp extends BaseJSPPanelApp {
+
+	@Override
+	public String getJspPath() {
+		return "/liferay_book/info.jsp";
+	}
 
 	@Override
 	public Portlet getPortlet() {
@@ -27,9 +34,14 @@ public class LiferayCoursesPanelApp extends BasePanelApp {
 		return LiferayCoursesAdminPortletKeys.PORTLET_ID;
 	}
 
-	@Reference(
-		target = "(javax.portlet.name=" + LiferayCoursesAdminPortletKeys.PORTLET_ID + ")"
-	)
+	@Override
+	protected ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Reference(target = "(javax.portlet.name=" + LiferayCoursesAdminPortletKeys.PORTLET_ID + ")")
 	private Portlet _portlet;
+	@Reference(target = "(osgi.web.symbolicname=com.liferaybook.courses.web)")
+	private ServletContext _servletContext;
 
 }
---
+++ modules/liferay-courses-web/src/main/resources/META-INF/resources/liferay_book/info.jsp
@@ -0,0 +1,6 @@
+<%@ include file="../init.jsp" %>
+<li class="nav-item" role="presentation">
+    <a href="/web/liferay-book" class="nav-link" style="color: #eee; font-size: small;" target="_blank">
+        Go to Liferay Book Site <clay:icon symbol="shortcut" />
+    </a>
+</li>
