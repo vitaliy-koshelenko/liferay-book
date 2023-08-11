6206910b1e7175d5c42e3da529885f9c7a35ef0f
Vitaliy Koshelenko <vetal324@gmail.com>
2023-08-11 16:58:04 +0300

    12-permissions: missing service builder classes

---
+++ modules/liferay-courses-manager/liferay-courses-manager-service/src/main/java/com/liferaybook/courses/manager/service/http/CourseServiceHttp.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferaybook.courses.manager.service.http;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.auth.HttpPrincipal;
+import com.liferay.portal.kernel.service.http.TunnelUtil;
+import com.liferay.portal.kernel.util.MethodHandler;
+import com.liferay.portal.kernel.util.MethodKey;
+
+import com.liferaybook.courses.manager.service.CourseServiceUtil;
+
+/**
+ * Provides the HTTP utility for the
+ * <code>CourseServiceUtil</code> service
+ * utility. The
+ * static methods of this class calls the same methods of the service utility.
+ * However, the signatures are different because it requires an additional
+ * <code>HttpPrincipal</code> parameter.
+ *
+ * <p>
+ * The benefits of using the HTTP utility is that it is fast and allows for
+ * tunneling without the cost of serializing to text. The drawback is that it
+ * only works with Java.
+ * </p>
+ *
+ * <p>
+ * Set the property <b>tunnel.servlet.hosts.allowed</b> in portal.properties to
+ * configure security.
+ * </p>
+ *
+ * <p>
+ * The HTTP utility is only generated for remote services.
+ * </p>
+ *
+ * @author Vitaliy Koshelenko
+ * @generated
+ */
+public class CourseServiceHttp {
+
+	public static com.liferaybook.courses.manager.model.Course getCourse(
+			HttpPrincipal httpPrincipal, long courseId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		try {
+			MethodKey methodKey = new MethodKey(
+				CourseServiceUtil.class, "getCourse",
+				_getCourseParameterTypes0);
+
+			MethodHandler methodHandler = new MethodHandler(
+				methodKey, courseId);
+
+			Object returnObj = null;
+
+			try {
+				returnObj = TunnelUtil.invoke(httpPrincipal, methodHandler);
+			}
+			catch (Exception exception) {
+				if (exception instanceof
+						com.liferay.portal.kernel.exception.PortalException) {
+
+					throw (com.liferay.portal.kernel.exception.PortalException)
+						exception;
+				}
+
+				throw new com.liferay.portal.kernel.exception.SystemException(
+					exception);
+			}
+
+			return (com.liferaybook.courses.manager.model.Course)returnObj;
+		}
+		catch (com.liferay.portal.kernel.exception.SystemException
+					systemException) {
+
+			_log.error(systemException, systemException);
+
+			throw systemException;
+		}
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(CourseServiceHttp.class);
+
+	private static final Class<?>[] _getCourseParameterTypes0 = new Class[] {
+		long.class
+	};
+
+}
